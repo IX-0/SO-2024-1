f3c2850ca667f50dc00dff6b47f31c15e87ff2fb
Goncsal <goncalodfalmeida1@gmail.com>
2024-10-19 11:53:39 +0100

    Added Function for regexChecking.All functions in backup_files.sh for now.

--- backup_files.sh
+++ backup_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# leaving the functions for the flags here, will change later.
+
+
 backup(){
-    if [ ! -d $1 ];then
+    if [[] ! -d $1 ]];then
         echo "no such work diretory to backup"; #If workdir doesnt exist, exit immediatly
         exit 1 # function failed
     fi
@@ -10,8 +13,8 @@ backup(){
     updated=0;
     warnings=0;
     for file in *;do
-        if [ -e "$2/$file" ];then
-            if [ $file -nt $2/$file ];then
+        if [[ -e "$2/$file" ]];then
+            if [[ $file -nt $2/$file ]];then
                 cp -a $file "$2/$file";
                 (( updated++ ));
             elif [[ $2/$file -nt $file ]]; then #throw warning
@@ -36,7 +39,27 @@ checking(){
 }
 #In this version of the script, seeing that $0 is either -c or the workdir, this works perfectly. Verifying the order of the flags and args must be done in backup.sh though. And then combine it with these functions here.
 
-if [ "$1" == '-c' ]; then
+checkRegex(){
+    regex_pattern="$1"
+    filename="$2"
+    if [[ $filename =~ $regex_pattern ]]
+        exit 0; #sucess, get it using '$?'
+    fi
+    exit 1; #failed
+}
+# is there a better way than to check for each file if regex_pattern matches its name?
+#  find /path/to/dir -type f | grep -E '^[a-zA-Z0-9_-]+\.txt$'
+#  returns the pathnames of files that match the regex_pattern. Might not be the best option, seeing we still 
+#  need to iterate over files anyways. 
+#  In 500 files, if only one matches the pattern, i see it being better. idk
+
+excludeFiles(){ #check if it is in the list? 
+#are we supposed to delete files that are in the list and also already in the backup diretory?
+               
+
+}
+
+if [[ "$1" == '-c' ]]; then
     checking
     backup "$2" "$3"
 else
