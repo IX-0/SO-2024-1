d208281a5596a1810a25d3a9be17abe8de71eec7
Goncsal <goncalodfalmeida1@gmail.com>
2024-10-21 10:28:50 +0100

    backup_files.sh almost done

--- backup_files.sh
+++ backup_files.sh
@@ -1,25 +1,30 @@
 #!/bin/bash     
 
-workdir=$(realpath "$1")
-backupdir=$(realpath "$2")
 
-if [[] ! -d $workdir ]];then
-    echo "no such work diretory to backup"; #If workdir doesnt exist, exit immediatly
-    exit 1;
+if [[ ! -d $1 ]]
+then
+    echo "no such work diretory to backup" #If workdir doesnt exist, exit immediatly
+    exit 1
+elif [[ ! -d $2 ]]; then
+    mkdir $2
 fi
-for file in $workdir;do
-    if [[ -e "$2
-        $file" ]];then
-        if [[ $file -nt $backupdir/$file ]];then
-            cp -a $file "$backupdir/$file";
+backupdir=$(realpath "$2")
+workdir=$(realpath "$1")
+for file in $workdir
+    do
+    if [[ -e "$backupdir/$file" ]]
+    then
+        if [[ $file -nt $backupdir/$file ]]
+        then
+            cp -a $file "$backupdir/$file"
         else
             echo "WARNING: backup entry $2/$file is newer than $file; SHould not happen"
-            continue; 
+            continue
         fi
     else
-        cp -a $file "$backupdir/$file";
+        cp -a $file "$backupdir/$file"
     fi 
-done;
+done
 
 
 
