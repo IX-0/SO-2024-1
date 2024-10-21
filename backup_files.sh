d93ba31a57b068d8e4e6c1666bac0f5944cc3915
Igor Baltarejo <150348783+IX-0@users.noreply.github.com>
2024-10-21 11:54:16 +0100

    Stuff by igor

--- backup_files.sh
+++ backup_files.sh
@@ -1,30 +1,72 @@
 #!/bin/bash     
 
+#Ensure LOCALE is set to C for compatibility
+export LC_ALL=C
+
+#Vars
+_checking=false
+_help=false
+
+#Argument and flag parsing
+while getopts ":ch" flag
+do 
+    case $flag in
+        c) 
+            _checking=true ;;
+        h) 
+            _help=true ;;
+        ?)
+            echo "Invalid option -$OPTARG: aborting backup"
+            exit 1 ;;
+    esac
+done
+
+#Strip flags and argument flags from argument list
+shift $(($OPTIND - 1))
+
+if $_help
+then
+    echo "Usage: backup_files [-c] workingDir backupDir"
+    exit 0
+fi
 
 if [[ ! -d $1 ]]
 then
     echo "no such work diretory to backup" #If workdir doesnt exist, exit immediatly
     exit 1
-elif [[ ! -d $2 ]]; then
-    mkdir $2
 fi
-backupdir=$(realpath "$2")
+
+#Resolve full paths
 workdir=$(realpath "$1")
+backupdir=$(realpath "$2")
+
+#Check if backupDir is a subdirectory of workingDir
+if [[  "${backupdir##$workdir}" != "$backupdir" ]]
+then
+    echo "Error: Backup directory is a sub-directory of working directory"
+    exit 1
+fi
+
+#Create backupDir if needed
+if [[ ! -d "$backupdir" ]]
+then
+    mkdirHelper $backupdir
+fi
+
 for file in $workdir
-    do
+do
     if [[ -e "$backupdir/$file" ]]
     then
         if [[ $file -nt $backupdir/$file ]]
         then
-            cp -a $file "$backupdir/$file"
+            cpHelper $file "$backupdir/$file"
         else
             echo "WARNING: backup entry $2/$file is newer than $file; SHould not happen"
             continue
         fi
     else
-        cp -a $file "$backupdir/$file"
+        cpHelper $file "$backupdir/$file"
     fi 
 done
 
-
-
+exit 0 #Made with love by Igor Baltarejo & Gonçalo Almeida
